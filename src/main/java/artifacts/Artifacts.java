8e1e917adf0ba029f837ad832dce5d3d1c27d66c
Florens Pauwels <github@florens.be>
2020-07-27 15:57:30 +0200

    Start ModInitializer & Fix creative tab

--- src/main/java/artifacts/Artifacts.java
+++ src/main/java/artifacts/Artifacts.java
@@ -1,10 +1,9 @@
 package artifacts;
 
 import artifacts.client.render.MimicRenderer;
-import artifacts.common.config.Config;
 import artifacts.common.init.*;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
@@ -14,38 +13,20 @@ import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.Feature;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import top.theillusivec4.curios.api.SlotTypeMessage;
 import top.theillusivec4.curios.api.SlotTypePreset;
 
-@Mod("artifacts")
-@SuppressWarnings("unused")
-public class Artifacts {
+public class Artifacts implements ModInitializer {
 
     public static final String MODID = "artifacts";
 
-    public static final ItemGroup CREATIVE_TAB = new ItemGroup(MODID) {
-        @Override
-        @Environment(EnvType.CLIENT)
-        public ItemStack createIcon() {
-            return new ItemStack(Items.PLASTIC_DRINKING_HAT);
-        }
-    };
+    public static final ItemGroup CREATIVE_TAB = FabricItemGroupBuilder.build(
+            new Identifier(MODID, "item_group"),
+            () -> new ItemStack(Items.PLASTIC_DRINKING_HAT)
+    );
 
-    public Artifacts() {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_SPEC);
+    @Override
+    public void onInitialize() {
+
     }
 
     @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
