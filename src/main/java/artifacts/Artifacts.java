d68fe027b806ec71986eb3f89bbbcd4c7ff5adda
Florens Pauwels <github@florens.be>
2020-07-27 22:41:57 +0200

    LootTables setup

--- src/main/java/artifacts/Artifacts.java
+++ src/main/java/artifacts/Artifacts.java
@@ -5,6 +5,7 @@ import artifacts.common.init.*;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.PlayerScreenHandler;
@@ -24,11 +25,15 @@ public class Artifacts implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        // Curios setup
         SlotTypePreset[] types = {SlotTypePreset.HEAD, SlotTypePreset.NECKLACE, SlotTypePreset.BELT};
         for (SlotTypePreset type : types) {
             CuriosApi.enqueueSlotType(SlotTypeInfo.BuildScheme.REGISTER, type.getInfoBuilder().build());
         }
         CuriosApi.enqueueSlotType(SlotTypeInfo.BuildScheme.REGISTER, SlotTypePreset.HANDS.getInfoBuilder().size(2).build());
         CuriosApi.enqueueSlotType(SlotTypeInfo.BuildScheme.REGISTER, new SlotTypeInfo.Builder("feet").priority(220).icon(PlayerScreenHandler.EMPTY_BOOTS_SLOT_TEXTURE).build());
+
+        // LootTable setup
+        LootTableLoadingCallback.EVENT.register(LootTables::onLootTableLoad);
     }
 }
--- src/main/java/artifacts/common/init/LootTables.java
+++ src/main/java/artifacts/common/init/LootTables.java
@@ -1,74 +1,62 @@
 package artifacts.common.init;
 
 import artifacts.Artifacts;
+import net.fabricmc.fabric.api.loot.v1.FabricLootSupplierBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.minecraft.loot.LootManager;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.entry.LootTableEntry;
+import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
-import net.minecraftforge.event.LootTableLoadEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
 import java.util.Arrays;
 import java.util.List;
 
+// TODO: probably doesn't work
 public class LootTables {
-
     public static final Identifier MIMIC = new Identifier(Artifacts.MOD_ID, "entities/mimic");
     public static final Identifier CAMPSITE_CHEST = new Identifier(Artifacts.MOD_ID, "chests/campsite_chest");
 
-    @Mod.EventBusSubscriber(modid = Artifacts.MOD_ID)
-    public static class LootTableEvents {
+    public static final List<Identifier> LOOT_TABLE_IDS = Arrays.asList(
+            new Identifier("minecraft", "chests/village/village_armorer"),
+            new Identifier("minecraft", "chests/village/village_butcher"),
+            new Identifier("minecraft", "chests/village/village_tannery"),
+            new Identifier("minecraft", "chests/village/village_temple"),
+            new Identifier("minecraft", "chests/village/village_toolsmith"),
+            new Identifier("minecraft", "chests/village/village_weaponsmith"),
+            new Identifier("minecraft", "chests/abandoned_mineshaft"),
+            new Identifier("minecraft", "chests/bastion_hoglin_stable"),
+            new Identifier("minecraft", "chests/bastion_treasure"),
+            new Identifier("minecraft", "chests/buried_treasure"),
+            new Identifier("minecraft", "chests/desert_pyramid"),
+            new Identifier("minecraft", "chests/end_city_treasure"),
+            new Identifier("minecraft", "chests/jungle_temple"),
+            new Identifier("minecraft", "chests/nether_bridge"),
+            new Identifier("minecraft", "chests/pillager_outpost"),
+            new Identifier("minecraft", "chests/ruined_portal"),
+            new Identifier("minecraft", "chests/shipwreck_treasure"),
+            new Identifier("minecraft", "chests/spawn_bonus_chest"),
+            new Identifier("minecraft", "chests/stronghold_corridor"),
+            new Identifier("minecraft", "chests/underwater_ruin_big"),
+            new Identifier("minecraft", "chests/woodland_mansion"),
+            new Identifier("minecraft", "entities/cow")
+    );
 
-        public static final List<String> LOOT_TABLE_LOCATIONS = Arrays.asList(
-                "chests/village/village_armorer",
-                "chests/village/village_butcher",
-                "chests/village/village_tannery",
-                "chests/village/village_temple",
-                "chests/village/village_toolsmith",
-                "chests/village/village_weaponsmith",
-                "chests/abandoned_mineshaft",
-                "chests/bastion_hoglin_stable",
-                "chests/bastion_treasure",
-                "chests/buried_treasure",
-                "chests/desert_pyramid",
-                "chests/end_city_treasure",
-                "chests/jungle_temple",
-                "chests/nether_bridge",
-                "chests/pillager_outpost",
-                "chests/ruined_portal",
-                "chests/shipwreck_treasure",
-                "chests/spawn_bonus_chest",
-                "chests/stronghold_corridor",
-                "chests/underwater_ruin_big",
-                "chests/woodland_mansion",
-                "entities/cow"
-        );
-
-        @SubscribeEvent
-        @SuppressWarnings("unused")
-        public static void onLootTableLoad(LootTableLoadEvent event) {
-            String prefix = "minecraft:";
-            String name = event.getName().toString();
-
-            if (name.startsWith(prefix)) {
-                String location = name.substring(name.indexOf(prefix) + prefix.length());
-                if (LOOT_TABLE_LOCATIONS.contains(location)) {
-                    event.getTable().addPool(getInjectPool(location));
-                }
-            }
+    public static void onLootTableLoad(ResourceManager resourceManager, LootManager manager, Identifier id, FabricLootSupplierBuilder supplier, LootTableLoadingCallback.LootTableSetter setter) {
+        if (LOOT_TABLE_IDS.contains(id)) {
+            supplier.withPool(getInjectPool(id.getPath()));
         }
+    }
 
-        public static LootPool getInjectPool(String entryName) {
-            return LootPool.builder()
-                    .with(getInjectEntry(entryName))
-                    .name("artifacts_inject")
-                    .build();
-        }
+    public static LootPool getInjectPool(String entryName) {
+        return LootPool.builder()
+                .with(getInjectEntry(entryName))
+                .build();
+    }
 
-        private static LootPoolEntry.Builder<?> getInjectEntry(String name) {
-            Identifier table = new Identifier(Artifacts.MOD_ID, "inject/" + name);
-            return LootTableEntry.builder(table).weight(1);
-        }
+    private static LootPoolEntry.Builder<?> getInjectEntry(String name) {
+        Identifier table = new Identifier(Artifacts.MOD_ID, "inject/" + name);
+        return LootTableEntry.builder(table).weight(1);
     }
 }
