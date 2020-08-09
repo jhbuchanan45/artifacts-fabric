900743d5c532ea5f4e00db46e75ad8d4b30d5716
Florens Pauwels <mail@florens.be>
2020-08-09 16:17:52 +0200

    Update Antidote Vessel model & textures

--- src/main/java/artifacts/client/render/model/curio/AntidoteVesselModel.java
+++ src/main/java/artifacts/client/render/model/curio/AntidoteVesselModel.java
@@ -1,48 +1,29 @@
 package artifacts.client.render.model.curio;
 
 import net.minecraft.client.model.ModelPart;
-import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
 
 public class AntidoteVesselModel extends BipedEntityModel<LivingEntity> {
 
-	public final ModelPart belt;
-	public final ModelPart jar;
-
 	public AntidoteVesselModel() {
-		super(0.5F, 0, 64, 32);
-
-		setVisible(false);
+		super(0.5F, 0, 32, 32);
 
 		torso = new ModelPart(this, 0, 0);
-		belt = new ModelPart(this, 0, 0);
-		jar = new ModelPart(this, 0, 16);
+		ModelPart jar = new ModelPart(this, 0, 16);
+		ModelPart lid = new ModelPart(this, 0, 26);
 
-		ModelPart lid = new ModelPart(this, 24, 0);
+		torso.addCuboid(-4, 0, -2, 8, 12, 4, 0.5F);
 
-		belt.addCuboid(-4, 0, -2, 8, 12, 4, 0.5F);
-
-		jar.addCuboid(0, 0, 0, 6, 8, 6);
-		lid.addCuboid(1, -1, 1, 4, 1, 4);
-
-		jar.setPivot(6, 18, -10);
+		jar.addCuboid(-2, 0, -2, 4, 6, 4);
+		lid.addCuboid(-1, -1, -1, 2, 1, 2);
+		jar.setPivot(4, 9, -3);
 		jar.yaw = -0.5F;
 
 		jar.addChild(lid);
-		torso.addChild(belt);
 		torso.addChild(jar);
-	}
 
-	@Override
-	public void render(MatrixStack matrixStackIn, VertexConsumer bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
-		jar.visible = false;
-		torso.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
-		jar.visible = true;
-		matrixStackIn.scale(0.5F, 0.5F, 0.5F);
-		belt.visible = false;
-		torso.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
-		belt.visible = true;
+		setVisible(false);
+		torso.visible = true;
 	}
 }
