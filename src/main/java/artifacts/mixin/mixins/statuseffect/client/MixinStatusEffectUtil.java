19c66697059fc2fdce77a2a3b4949b179b5b9634
Florens Pauwels <mail@florens.be>
2020-11-09 19:01:13 +0100

    Show which Artifact is giving the status effect on status effect card Replaces the duration

---
+++ src/main/java/artifacts/mixin/mixins/statuseffect/client/MixinStatusEffectUtil.java
@@ -0,0 +1,35 @@
+package artifacts.mixin.mixins.statuseffect.client;
+
+import artifacts.common.item.trinket.TrinketArtifactItem;
+import artifacts.common.trinkets.TrinketsHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffectUtil;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(StatusEffectUtil.class)
+public abstract class MixinStatusEffectUtil {
+
+	// TODO: There might be a better place for this
+	@Inject(method = "durationToString", at = @At("HEAD"), cancellable = true)
+	private static void setFromArtifactString(StatusEffectInstance effect, float multiplier, CallbackInfoReturnable<String> info) {
+		ClientPlayerEntity player = MinecraftClient.getInstance().player;
+
+		if (player != null && effect.isPermanent()) {
+			TrinketsHelper.getAllEquipped(player).forEach(stack -> {
+
+				if (!stack.isEmpty() && stack.getItem() instanceof TrinketArtifactItem) {
+					StatusEffectInstance trinketEffect = ((TrinketArtifactItem) stack.getItem()).getPermanentEffect();
+
+					if (trinketEffect != null && trinketEffect.getEffectType() == effect.getEffectType()) {
+						info.setReturnValue(stack.getName().getString());
+					}
+				}
+			});
+		}
+	}
+}
