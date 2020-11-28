5e00a25314aad2effcedafb61aa2f7d7e34b3152
Florens Pauwels <github@florens.be>
2020-11-28 16:51:56 +0100

    Revert "HandleFallDamageCallback for bunny hoppers and cloud in a bottle"
    This reverts commit a4c12490

--- src/main/java/artifacts/mixin/mixins/event/MixinLivingEntity.java
+++ src/main/java/artifacts/mixin/mixins/event/MixinLivingEntity.java
@@ -1,21 +1,16 @@
 package artifacts.mixin.mixins.event;
 
-import artifacts.common.events.HandleFallDamageCallback;
 import artifacts.common.events.LivingEntitySprintingCallback;
 import artifacts.common.events.PlayHurtSoundCallback;
-import artifacts.common.init.Items;
-import artifacts.common.trinkets.TrinketsHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.util.ActionResult;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity extends Entity {
@@ -36,14 +31,5 @@ public abstract class MixinLivingEntity extends Entity {
 	@Inject(method = "setSprinting", at = @At("TAIL"))
 	private void onSetSprinting(boolean sprinting, CallbackInfo info) {
 		LivingEntitySprintingCallback.EVENT.invoker().setSprinting((LivingEntity)(Object) this, sprinting);
-	}
-
-	@Inject(method = "handleFallDamage", cancellable = true, at = @At("HEAD"))
-	private void onHandleFallDamage(float fallDistance, float damageMultiplier, CallbackInfoReturnable<Boolean> info) {
-		ActionResult result = HandleFallDamageCallback.EVENT.invoker().handle((LivingEntity)(Object) this, fallDistance, damageMultiplier);
-
-		if (result == ActionResult.FAIL) {
-			info.setReturnValue(false);
-		}
 	}
 }
