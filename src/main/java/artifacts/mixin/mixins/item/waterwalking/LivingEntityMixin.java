2f79adcc072c8b33eab83f9bb9b0a96155cc648a
Florens Pauwels <github@florens.be>
2021-01-23 15:14:56 +0100

    Fix mixin

--- src/main/java/artifacts/mixin/mixins/item/waterwalking/LivingEntityMixin.java
+++ src/main/java/artifacts/mixin/mixins/item/waterwalking/LivingEntityMixin.java
@@ -1,4 +1,4 @@
-package artifacts.mixins.item.waterwalking;
+package artifacts.mixin.mixins.item.waterwalking;
 
 import artifacts.common.init.Items;
 import artifacts.common.trinkets.TrinketsHelper;
@@ -13,9 +13,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
-public abstract class MixinLivingEntity extends Entity {
+public abstract class LivingEntityMixin extends Entity {
 
-	public MixinLivingEntity(EntityType<?> type, World world) {
+	public LivingEntityMixin(EntityType<?> type, World world) {
 		super(type, world);
 	}
 
