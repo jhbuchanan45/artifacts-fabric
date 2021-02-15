7dd8f6d82631691454f463993402a74d3717abdf
Florens Pauwels <github@florens.be>
2021-02-16 00:21:46 +0100

    Fix slim claw textures

--- src/main/java/artifacts/item/trinket/glove/FeralClawsItem.java
+++ src/main/java/artifacts/item/trinket/glove/FeralClawsItem.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 public class FeralClawsItem extends GloveItem {
 
 	private static final Identifier TEXTURE_DEFAULT = new Identifier(Artifacts.MODID, "textures/entity/trinket/feral_claws_default.png");
-	private static final Identifier TEXTURE_SLIM = new Identifier(Artifacts.MODID, "textures/entity/trinket/feral_claws_default.png");
+	private static final Identifier TEXTURE_SLIM = new Identifier(Artifacts.MODID, "textures/entity/trinket/feral_claws_slim.png");
 
 	public static final EntityAttributeModifier ATTACK_SPEED_MODIFIER = new EntityAttributeModifier(UUID.fromString("7a3367b2-0a38-491d-b5c7-338d5d0c1dd4"), Artifacts.MODID + ":feral_claws_attack_speed", 1, EntityAttributeModifier.Operation.MULTIPLY_TOTAL);
 
