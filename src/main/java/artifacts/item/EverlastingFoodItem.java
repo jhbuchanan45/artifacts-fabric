d620f7603090752fc83ac6c4e1e24a6d107749e4
Florens Pauwels <github@florens.be>
2021-02-16 15:27:55 +0100

    Fix compatibility issues with everlasting foods

--- src/main/java/artifacts/item/EverlastingFoodItem.java
+++ src/main/java/artifacts/item/EverlastingFoodItem.java
@@ -3,24 +3,24 @@ package artifacts.item;
 import artifacts.Artifacts;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class EverlastingFoodItem extends ArtifactItem {
 
-	public EverlastingFoodItem(Settings settings) {
-		super(settings);
+	public EverlastingFoodItem(FoodComponent foodComponent) {
+		super(new Settings().food(foodComponent));
 	}
 
 	@Override
 	public ItemStack finishUsing(ItemStack stack, World world, LivingEntity entity) {
-		if (isFood()) {
-			entity.eatFood(world, stack.copy());
-			if (!world.isClient && entity instanceof PlayerEntity) {
-				((PlayerEntity) entity).getItemCooldownManager().set(this, Artifacts.CONFIG.general.everlastingFoodCooldown);
-			}
+		if (!world.isClient && entity instanceof PlayerEntity) {
+			((PlayerEntity) entity).getItemCooldownManager().set(this, Artifacts.CONFIG.general.everlastingFoodCooldown);
 		}
-		return stack;
+
+		// Stack decrement is cancelled in LivingEntity.eatFood() mixin
+		return super.finishUsing(stack, world, entity);
 	}
 
 	@Override
---
+++ src/main/java/artifacts/mixin/mixins/item/everlastingfood/LivingEntityMixin.java
@@ -0,0 +1,21 @@
+package artifacts.mixin.mixins.item.everlastingfood;
+
+import artifacts.item.EverlastingFoodItem;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+
+	@ModifyArgs(method = "eatFood", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;decrement(I)V"))
+	private void cancelDecrement(Args args, World world, ItemStack stack) {
+		if (stack.getItem() instanceof EverlastingFoodItem) {
+			args.set(0, 0);
+		}
+	}
+}
