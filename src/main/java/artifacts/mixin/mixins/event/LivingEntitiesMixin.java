67ebac89b67f9e54204c3056b15091e0d4a88211
Florens Pauwels <github@florens.be>
2021-02-17 15:26:36 +0100

    Use correct OpCodes

--- src/main/java/artifacts/mixin/mixins/event/LivingEntitiesMixin.java
+++ src/main/java/artifacts/mixin/mixins/event/LivingEntitiesMixin.java
@@ -2,13 +2,13 @@ package artifacts.mixin.mixins.event;
 
 import artifacts.events.LivingEntityDamagedCallback;
 import artifacts.events.LivingEntityHurtCallback;
-import jdk.internal.org.objectweb.asm.Opcodes;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
