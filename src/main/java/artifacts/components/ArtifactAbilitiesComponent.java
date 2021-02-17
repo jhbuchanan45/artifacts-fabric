95c8f9a76917e246ac814bddd892ac198dd00ac5
Florens Pauwels <github@florens.be>
2021-02-17 17:50:53 +0100

    Artifact abilities component

---
+++ src/main/java/artifacts/components/ArtifactAbilitiesComponent.java
@@ -0,0 +1,42 @@
+package artifacts.components;
+
+import dev.onyxstudios.cca.api.v3.component.sync.AutoSyncedComponent;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+public class ArtifactAbilitiesComponent implements AutoSyncedComponent {
+
+	private final PlayerEntity player;
+	private boolean isAirSwimming;
+
+	public ArtifactAbilitiesComponent(PlayerEntity player) {
+		this.player = player;
+	}
+
+	@Override
+	public void readFromNbt(CompoundTag tag) {
+		this.isAirSwimming = tag.getBoolean("isAirSwimming");
+	}
+
+	@Override
+	public void writeToNbt(CompoundTag tag) {
+		tag.putBoolean("isAirSwimming", this.isAirSwimming);
+	}
+
+	@Override
+	public void applySyncPacket(PacketByteBuf buf) {
+		this.isAirSwimming = buf.readBoolean();
+	}
+
+	@Override
+	public void writeSyncPacket(PacketByteBuf buf, ServerPlayerEntity recipient) {
+		buf.writeBoolean(this.isAirSwimming);
+	}
+
+	@Override
+	public boolean shouldSyncWith(ServerPlayerEntity player) {
+		return player.getUuid().equals(this.player.getUuid());
+	}
+}
--- src/main/java/artifacts/init/Components.java
+++ src/main/java/artifacts/init/Components.java
@@ -2,6 +2,7 @@ package artifacts.init;
 
 import artifacts.Artifacts;
 import artifacts.components.BooleanComponent;
+import artifacts.components.ArtifactAbilitiesComponent;
 import artifacts.components.TrinketEnabledComponent;
 import artifacts.item.trinket.TrinketArtifactItem;
 import dev.onyxstudios.cca.api.v3.component.ComponentKey;
@@ -10,6 +11,7 @@ import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
 import dev.onyxstudios.cca.api.v3.item.ItemComponentFactoryRegistry;
 import dev.onyxstudios.cca.api.v3.item.ItemComponentInitializer;
+import nerdhub.cardinal.components.api.util.RespawnCopyStrategy;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.util.Identifier;
 
@@ -21,10 +23,14 @@ public class Components implements EntityComponentInitializer, ItemComponentInit
 	public static final ComponentKey<TrinketEnabledComponent> TRINKET_ENABLED =
 			ComponentRegistryV3.INSTANCE.getOrCreate(new Identifier(Artifacts.MODID, "trinket_enabled"),
 					TrinketEnabledComponent.class);
+	public static final ComponentKey<ArtifactAbilitiesComponent> ARTIFACT_ABILITIES =
+			ComponentRegistryV3.INSTANCE.getOrCreate(new Identifier(Artifacts.MODID, "artifact_abilities"),
+					ArtifactAbilitiesComponent.class);
 
 	@Override
 	public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
 		registry.registerFor(ItemEntity.class, DROPPED_ITEM_ENTITY, entity -> new BooleanComponent("wasDropped"));
+		registry.registerForPlayers(ARTIFACT_ABILITIES, ArtifactAbilitiesComponent::new, RespawnCopyStrategy.LOSSLESS_ONLY);
 	}
 
 	@Override
