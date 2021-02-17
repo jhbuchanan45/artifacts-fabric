9f2e4b0c00b84303f95db477c48d846bf8866030
Florens Pauwels <github@florens.be>
2021-02-17 13:49:30 +0100

    Add onEntityDamaged event

---
+++ src/main/java/artifacts/events/LivingEntityDamagedCallback.java
@@ -0,0 +1,23 @@
+package artifacts.events;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+
+/**
+ * Callback for applying Trinket effects when the wearer was damaged, same injection point as Forge's LivingDamagedEvent
+ */
+public interface LivingEntityDamagedCallback {
+
+	Event<LivingEntityDamagedCallback> EVENT = EventFactory.createArrayBacked(LivingEntityDamagedCallback.class,
+			(listeners) -> (entity, attacker, random) -> {
+				if (entity != null && attacker != null) {
+					for (LivingEntityDamagedCallback listener : listeners) {
+						listener.damage(entity, attacker, random);
+					}
+				}
+			});
+
+	void damage(LivingEntity entity, DamageSource source, float amount);
+}
--- src/main/java/artifacts/mixin/mixins/event/LivingEntitiesMixin.java
+++ src/main/java/artifacts/mixin/mixins/event/LivingEntitiesMixin.java
@@ -1,6 +1,8 @@
 package artifacts.mixin.mixins.event;
 
+import artifacts.events.LivingEntityDamagedCallback;
 import artifacts.events.LivingEntityHurtCallback;
+import jdk.internal.org.objectweb.asm.Opcodes;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -20,9 +22,16 @@ public abstract class LivingEntitiesMixin extends Entity {
 	}
 
 	@Inject(method = "applyDamage", at = @At("HEAD"))
-	private void onUserHurt(DamageSource source, float amount, CallbackInfo info) {
+	private void onEntityHurt(DamageSource source, float amount, CallbackInfo info) {
 		if (!this.isInvulnerableTo(source)) {
 			LivingEntityHurtCallback.EVENT.invoker().hurt((LivingEntity) (Object) this, source, amount);
+		}
+	}
+
+	@Inject(method = "applyDamage", allow = 1, at = @At(value = "JUMP", opcode = Opcodes.IFNE))
+	private void onEntityDamaged(DamageSource source, float amount, CallbackInfo info) {
+		if (!this.isInvulnerableTo(source)) {
+			LivingEntityDamagedCallback.EVENT.invoker().damage((LivingEntity) (Object) this, source, amount);
 		}
 	}
 }
