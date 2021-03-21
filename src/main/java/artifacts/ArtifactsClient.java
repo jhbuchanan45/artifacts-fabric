dc8ffb24fa3ce06fc90fca62813b443a5c79b98e
Florens Pauwels <github@florens.be>
2021-03-21 10:56:30 +0100

    Use Fabric API to add baked model
    Possibly fixes optifine crash

--- src/main/java/artifacts/ArtifactsClient.java
+++ src/main/java/artifacts/ArtifactsClient.java
@@ -6,17 +6,25 @@ import artifacts.init.Items;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
+import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
 public class ArtifactsClient implements ClientModInitializer {
 
+	private static final ModelIdentifier UMBRELLA_HELD_MODEL = new ModelIdentifier(
+			new Identifier(Artifacts.MODID, "umbrella_in_hand"), "inventory");
+
 	@Override
 	public void onInitializeClient() {
 		// Mimic EntityRenderer
 		EntityRendererRegistry.INSTANCE.register(Entities.MIMIC, (dispatcher, context) -> new MimicRenderer(dispatcher));
+
+		// Held Umbrella model
+		ModelLoadingRegistry.INSTANCE.registerModelProvider((manager, out) -> out.accept(UMBRELLA_HELD_MODEL));
 
 		// ModelPredicateProvider for rendering of umbrella blocking
 		FabricModelPredicateProviderRegistry.register(Items.UMBRELLA, new Identifier("blocking"), (stack, world, entity)
